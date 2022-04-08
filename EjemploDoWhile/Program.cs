0d5a6be203121d8adb125aca89fc8c0c8aabc7b3
Roberto Andrade Salazar <andrade.roberto@uabc.edu.mx>
2022-04-07 19:07:16 -0700

    PracticaDoWhile

---
+++ EjemploDoWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 15;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+                Console.ReadKey();
+            }
+            while (i <= 10);
+            }
+    }
+}
